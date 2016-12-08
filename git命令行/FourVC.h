2fe7d2bac054736e98f5a2c35babfdb6e48d88bd
聂银龙 <nieyinlong@MacBook-Pro.local>
2016-12-08 22:00:20 +0800

    提交fourVC

---
+++ git命令行/FourVC.h
@@ -0,0 +1,13 @@
+//
+//  FourVC.h
+//  git命令行
+//
+//  Created by 聂银龙 on 16/12/8.
+//  Copyright © 2016年 Jiuzhekan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface FourVC : ViewController
+
+@end
---
+++ git命令行/FourVC.m
@@ -0,0 +1,38 @@
+//
+//  FourVC.m
+//  git命令行
+//
+//  Created by 聂银龙 on 16/12/8.
+//  Copyright © 2016年 Jiuzhekan. All rights reserved.
+//
+
+#import "FourVC.h"
+
+@interface FourVC ()
+
+@end
+
+@implementation FourVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"新增 fourVC");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
